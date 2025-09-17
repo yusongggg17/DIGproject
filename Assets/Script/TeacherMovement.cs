c865f5d1f467c136d889e3ca229a466109935035
06 <user@FIH-COM23a106.fsu.edu>
2025-09-17 14:24:53 -0400

    added movements for the teacher

--- Assets/Script/TeacherMovement.cs
+++ Assets/Script/TeacherMovement.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class TeacherMovement: MonoBehaviour
 {
+    public float moveSpeed = 10f; 
+    private Vector3 moveDirection = Vector3.zero;
+    public int direction = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,14 @@ public class TeacherMovement: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = Vector3.right * direction * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Walls")) {
+            direction = -direction;
+        }
     }
 }
